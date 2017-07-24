3917f8df1b3200bb2cbd7b47108ded0f7432dc8c
mkruneva <marta.kruneva@gmail.com>
2017-07-24 16:53:10 +0100

    index.html loads three.js canvas size set to 4by3 - both in the parent html element and in the camera properties of the three.js csene

--- shoeDesignerThreeJS.js
+++ shoeDesignerThreeJS.js
@@ -1,5 +1,6 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 1000 );
+function init() {
+	var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 50, 4/3, 0.1, 1000 );
 
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 scene.add( directionalLight);
@@ -8,9 +9,12 @@ var light = new THREE.AmbientLight( 0xbfbfbf ); // soft white light
 scene.add( light );
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor( 0xdddddd, 1 );
 //var renderer = new THREE.WebGLRenderer({ antialias: true });
+var parent = document.getElementById('canvasContainer');
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.getElementById('canvasContainer').appendChild(renderer.domElement);
+//document.body.appendChild( renderer.domElement );
 
 // Initail Cube Geometry
 // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -52,3 +56,5 @@ var animate = function () {
 };
 
 animate();
+}
+
