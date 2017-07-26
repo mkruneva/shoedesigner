e9b07342e340e3b0246573055069d635093ea95a
mkruneva <marta.kruneva@gmail.com>
2017-07-26 13:17:26 +0100

    onclick functions loading different obj files

--- myScript.js
+++ myScript.js
@@ -1,5 +1,17 @@
-var objName = 'PC300AHPLAPL-simple.obj';
-// var objName = 'PE300SAPLAPL-simple.obj';
-var objPathName = 'obj/' + objName;  
+//var objName = 'PC300AHPLAPL-simple.obj';
+var objName = 'PE300SAPLAPL-simple.obj';
+ 
+
+function loadPC300AH() {
+	objName = 'PC300AHPLAPL-simple.obj';
+	console.log('objName = ', objName);
+}
+
+function loadPE300SA() {
+	objName = 'PE300SAPLAPL-simple.obj';
+	console.log('objName = ', objName);
+}
+
+var objPathName = 'obj/' + objName; 
 
 
