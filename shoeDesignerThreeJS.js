9d21f65580ea62e5af9011628f6c76cfcfcacd00
mkruneva <marta.kruneva@gmail.com>
2017-07-26 12:50:47 +0100

    if inside obj loader showing only the default meshes

--- shoeDesignerThreeJS.js
+++ shoeDesignerThreeJS.js
@@ -95,7 +95,18 @@ function init() {
 
 	    object.traverse( function ( child ) {
 	        if ( child instanceof THREE.Mesh ) {
+
+
+	        	// all meshes except for the default ones are hidden
+	        	var startMeshes = (child.name == 'FR1')||(child.name == 'FR2')||(child.name == 'HE1')||(child.name == 'HG1')||(child.name == 'HT1')||(child.name == 'IB1')||(child.name == 'IL1')||(child.name == 'IN1')||(child.name == 'LI1')||(child.name == 'LO1')||(child.name == 'SO1');
+	        	if (startMeshes) {
+	        		child.visible = true;
+	        	} else {
+	        		child.visible = false;
+	        	}
+
 	        	child.castShadow = true;
+
 	        	if (child.name == 'FR1') {
 				child.material = goldMaterial;
 				} else 
