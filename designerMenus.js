854ffe2a870f8136750d491b648523d5ef508559
mkruneva <marta.kruneva@gmail.com>
2017-07-29 19:58:06 +0100

    jquery manues slide up and down

--- designerMenus.js
+++ designerMenus.js
@@ -1,3 +1,5 @@
+// //JS only
+
 var menuNames = ['thumbCs', 'thumbHe', 'thumbFr', 'thumbBc', 'thumbSt', 'thumbEm', 'thumbCl'];
 
 function hideAllMenuDivs(namesArray) {
@@ -7,31 +9,31 @@ function hideAllMenuDivs(namesArray) {
 	}
 }
 
-function showHideMany(namesArray) {
-	for (var i = 0; i < namesArray.length; i++) {
-	var btnNameString = namesArray[i] + 'Btn';
-	var divNameString = namesArray[i] + 'Div';
-	btnName = document.getElementById(btnNameString);
-	divName = document.getElementById(divNameString);
-	showHideSingle(btnName, divName);
-	}
-}
+// function showHideMany(namesArray) {
+// 	for (var i = 0; i < namesArray.length; i++) {
+// 	var btnNameString = namesArray[i] + 'Btn';
+// 	var divNameString = namesArray[i] + 'Div';
+// 	btnName = document.getElementById(btnNameString);
+// 	divName = document.getElementById(divNameString);
+// 	showHideSingle(btnName, divName);
+// 	}
+// }
 
-function showHideSingle(btnName, divName) {
-	btnName.onclick = function () {
-		console.log('btnName = ', btnName);
-		if (divName.style.display === 'none') {
-				divName.style.display = 'block';
-		} else {
-			divName.style.display = 'none';
-		}
-	};	   
-}
+// function showHideSingle(btnName, divName) {
+// 	btnName.onclick = function () {
+// 		console.log('btnName = ', btnName);
+// 		if (divName.style.display === 'none') {
+// 				divName.style.display = 'block';
+// 		} else {
+// 			divName.style.display = 'none';
+// 		}
+// 	};	   
+// }
 
 hideAllMenuDivs(menuNames);
-showHideMany(menuNames);
+// showHideMany(menuNames);
 
-//JQ tests
+//JQuery 
 
 // $(document).ready(showHide(thumbCsBtn));
 // function showHide(nameOfButton) {
@@ -50,14 +52,80 @@ showHideMany(menuNames);
 //     });
 // }
 
-// // JQuery script example 
-// $(document).ready(showHide());
-// function showHide() {
-// 	$("#thumbCsBtn").click(function(){
-//     	if (($("#thumbCsDiv")[0]).style.display === 'none') {
-//     		$("#thumbCsDiv").slideDown(600);
-//     	} else {
-//     		$("#thumbCsDiv").slideUp(600);
-//     	}    
-//     });
-// }
+// JQuery script example 
+$(document).ready(showHideCs());
+function showHideCs() {
+	$("#thumbCsBtn").click(function(){
+    	if (($("#thumbCsDiv")[0]).style.display === 'none') {
+    		$("#thumbCsDiv").slideDown(500);
+    	} else {
+    		$("#thumbCsDiv").slideUp(500);
+    	}    
+    });
+}
+
+$(document).ready(showHideHe());
+function showHideHe() {
+	$("#thumbHeBtn").click(function(){
+    	if (($("#thumbHeDiv")[0]).style.display === 'none') {
+    		$("#thumbHeDiv").slideDown(500);
+    	} else {
+    		$("#thumbHeDiv").slideUp(500);
+    	}    
+    });
+}
+
+$(document).ready(showHideFr());
+function showHideFr() {
+	$("#thumbFrBtn").click(function(){
+    	if (($("#thumbFrDiv")[0]).style.display === 'none') {
+    		$("#thumbFrDiv").slideDown(500);
+    	} else {
+    		$("#thumbFrDiv").slideUp(500);
+    	}    
+    });
+}
+
+$(document).ready(showHideBc());
+function showHideBc() {
+	$("#thumbBcBtn").click(function(){
+    	if (($("#thumbBcDiv")[0]).style.display === 'none') {
+    		$("#thumbBcDiv").slideDown(500);
+    	} else {
+    		$("#thumbBcDiv").slideUp(500);
+    	}    
+    });
+}
+
+$(document).ready(showHideSt());
+function showHideSt() {
+	$("#thumbStBtn").click(function(){
+    	if (($("#thumbStDiv")[0]).style.display === 'none') {
+    		$("#thumbStDiv").slideDown(500);
+    	} else {
+    		$("#thumbStDiv").slideUp(500);
+    	}    
+    });
+}
+
+$(document).ready(showHideEm());
+function showHideEm() {
+	$("#thumbEmBtn").click(function(){
+    	if (($("#thumbEmDiv")[0]).style.display === 'none') {
+    		$("#thumbEmDiv").slideDown(500);
+    	} else {
+    		$("#thumbEmDiv").slideUp(500);
+    	}    
+    });
+}
+
+$(document).ready(showHideCl());
+function showHideCl() {
+	$("#thumbClBtn").click(function(){
+    	if (($("#thumbClDiv")[0]).style.display === 'none') {
+    		$("#thumbClDiv").slideDown(500);
+    	} else {
+    		$("#thumbClDiv").slideUp(500);
+    	}    
+    });
+}
