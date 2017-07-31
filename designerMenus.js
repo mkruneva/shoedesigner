418c9c2ee25f1c19e94f809fa016101caa27b83d
mkruneva <marta.kruneva@gmail.com>
2017-07-31 15:36:09 +0100

    jq menus and cleanup

--- designerMenus.js
+++ designerMenus.js
@@ -33,55 +33,17 @@
 // hideAllMenuDivs(menuNames);
 // showHideMany(menuNames);
 
-//JQuery 
-
-//jQ creating arrays 
-
-// var btns = $('#shoeMenu .btn');
-// var divs = $('.thumb-container');
-
-// for (var i = 0; i < divs.length; i++) {
-// 	divs.eq(i).hide();
-// }
-
-// for (var i = 0; i < btns.length; i++) {
-// 	console.log('(divs.eq(i)) is', (divs.eq(i)));
-// 	(btns.eq(i)).show();
-// 	(btns.eq(i)).click(function(){
-// 		console.log('(divs.eq(i)) is', (divs.eq(i)));
-//     });
-// }
-
-
-// $(document).ready(showHide(thumbCsBtn));
-// function showHide(nameOfButton) {
-// 	var idButton = nameOfButton.id;
-// 	console.log(idButton);
-// 	console.log(typeof(idButton));
-// 	console.log($("#idButton"));
-// 	console.log($("#thumbCsBtn"));
-
-// 	$("#thumbCsBtn").click(function(){
-//     	if (($("#thumbCsDiv")[0]).style.display === 'none') {
-//     		$("#thumbCsDiv").slideDown(600);
-//     	} else {
-//     		$("#thumbCsDiv").slideUp(600);
-//     	}    
-//     });
-// }
-
-// //JQuery script example 
-// $(document).ready(showHideCs());
-// function showHideCs() {
-// 	$("#thumbCsBtn").click(function(){
-//     	if (($("#thumbCsDiv")[0]).style.display === 'none') {
-//     		$("#thumbCsDiv").slideDown(500);
-//     	} else {
-//     		$("#thumbCsDiv").slideUp(500);
-//     	}    
-//     });
-// }
+//jQ
 
 var btns = $('#shoeMenu .btn');
-btns.click(function(e) {$(this).parent().find('.thumb-container').hide(1000);});
+$('.thumb-container:gt(0)').hide();
+
+btns.click(function() {
+	var div = $(this).parent().find('.thumb-container');
+	if ((div[0]).style.display === 'none') {
+		div.slideDown(500);
+	} else {
+		div.slideUp(500);
+	}
+});
 
