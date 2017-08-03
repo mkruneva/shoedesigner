440ed4a2f9f17f08f9a0cf6b8f0e72fb91b80329
mkruneva <marta.kruneva@gmail.com>
2017-08-03 14:42:37 +0100

    testing display of the elements

--- designerAdditional.js
+++ designerAdditional.js
@@ -23,52 +23,52 @@
 // var newObjPath = objPathFun(csAbr, heAbr, frAbr, baArb);
 // console.log('newObjPath = ', newObjPath);
 
-//FRONTS WITH CHILDREN
-var fr_pc = [
-    new ThumbObject('PLA', 'images/FRPC/FRPCPLA300AH.jpg', 'null', 'Plain', true, ba_cb, [0, 0, 0]),
-    new ThumbObject('PTO', 'images/FRPC/FRPCPTO300AH.jpg', 'null', 'Pointed Toe Cap', true, ba_cb, [0, 0, 1]),
-    new ThumbObject('TOE', 'images/FRPC/FRPCTOE300AH.jpg', 'null', 'Toe Cap', true, ba_cb, [0, 0, 2]),
-    new ThumbObject('PAN', 'images/FRPC/FRPCPAN300AH.jpg', 'null', 'Panneled', true, ba_cb, [0, 0, 3]),
-    new ThumbObject('PPT', 'images/FRPC/FRPCPPT300AH.jpg', 'null', 'Panneled Pointed Toe Cap', true, ba_cb, [0, 0, 4]),
-    new ThumbObject('PTC', 'images/FRPC/FRPCPTC300AH.jpg', 'null', 'Panneled Toe Cap', true, ba_cb, [0, 0, 5])
-];
+$(document).ready(function() {
+    var redrawMenu = function(selection) {
+    	//var displ = $('.thumb-container').map((i, e) => e.style.display).toArray(); //display style to be redrawn each time
+    	
 
-var fr_ph = [
-    new ThumbObject('PLA', 'images/FRPH/FRPHPLA300AH.jpg', 'null', 'Plain', true, ba_cb, [1, 0, 0]),
-    new ThumbObject('PTO', 'images/FRPH/FRPHPTO300AH.jpg', 'null', 'Pointed Toe Cap', true, ba_cb, [1, 0, 1]),
-    new ThumbObject('TOE', 'images/FRPH/FRPHTOE300AH.jpg', 'null', 'Toe Cap', true, ba_cb, [1, 0, 2])
-];
+        $('#shoeMenu').html(
+            thumbMany(cs, 'Core Shape') +
+            thumbMany(cs[selection[0]].children, "Heels") +
+            thumbMany(cs[selection[0]].children[selection[1]].children, "Fronts") +
+            thumbMany(cs[selection[0]].children[selection[1]].children[selection[2]].children, "Backs") +
+            thumbMany(cs[selection[0]].children[selection[1]].children[selection[2]].children[selection[3]].children, "Straps") +
+            thumbMany(cs[selection[0]].children[selection[1]].children[selection[2]].children[selection[3]].children[selection[4]].children, "Embellishments")
+        );
 
-var fr_po = [
-    new ThumbObject('PLA', 'images/FRPO/FRPOPLA300AH.jpg', 'null', 'Plain', true, ba_cb, [2, 0, 0])
-];
+        //$('.thumb-container').each((i, e) => e.style.display = displ[i]);  //display style to be redrawn each time
 
-var fr_ac = [
-    new ThumbObject('PLA', 'images/FRAC/FRACPLA300AH.jpg', 'null', 'Plain', true, ba_cb, [3, 0, 0]),
-    new ThumbObject('PTO', 'images/FRAC/FRACPTO300AH.jpg', 'null', 'Pointed Toe Cap', true, ba_cb, [0, 0, 1]),
-    new ThumbObject('TOE', 'images/FRAC/FRACTOE300AH.jpg', 'null', 'Toe Cap', true, ba_cb, [3, 0, 2]),
-    new ThumbObject('PAN', 'images/FRAC/FRACPAN300AH.jpg', 'null', 'Panneled', true, ba_cb, [3, 0, 3]),
-    new ThumbObject('PPT', 'images/FRAC/FRACPPT300AH.jpg', 'null', 'Panneled Pointed Toe Cap', true, ba_cb, [0, 0, 4]),
-    new ThumbObject('PTC', 'images/FRAC/FRACPTC300AH.jpg', 'null', 'Panneled Toe Cap', true, ba_cb, [3, 0, 5])
-];
+        //var thumbArray = $('.thumb-container');   // diplay only selected div 
+        //var thumbArray = thumbArray.toArray(); 
 
-var fr_pe = [
-    new ThumbObject('PLA', 'images/FRPE/FRPEPLA300AH.jpg', 'null', 'Plain', true, ba_cb, [4, 0, 0]),
-    new ThumbObject('PAN', 'images/FRPE/FRPEPAN300AH.jpg', 'null', 'Panneled', true, ba_cb, [4, 0, 1])
-];
+        var thumb = $('#shoeMenu').find('img');
+        thumb.click(function() {
+            var selection = $(this).attr("selection");
+            var selectionArray = selection.split(',').map(function(n) { return parseInt(n); });
+            redrawMenu(selectionArray);
 
-var fr_sa = [
-    new ThumbObject('STR', 'images/FRSA/FRSASTR300AH.jpg', 'null', 'Toe Cap', true, ba_cb, [5, 0, 0]),
-    new ThumbObject('PSA', 'images/FRSA/FRSAPSA300AH.jpg', 'null', 'Toe Cap', true, ba_cb, [5, 0, 1]),
-    new ThumbObject('COP', 'images/FRSA/FRSACOP300AH.jpg', 'null', 'Plain', true, ba_cb, [5, 0, 2]),
-    new ThumbObject('GLA', 'images/FRSA/FRSAGLA300AH.jpg', 'null', 'Pointed Toe Cap', true, ba_cb, [5, 0, 3])
-];
+            var div = $(this).parents().find('.thumb-container');
+            console.log('div is ' + div);
+            console.log('thumbArray is ' + thumbArray);
+            //console.log(thumbArray.indexOf(div));
+            
+       
+        });
 
-var fr_fl = [
-    new ThumbObject('PLA', 'images/FRFL/FRFLPLA.jpg', 'null', 'Plain', true, ba_fl, [6, 0, 0]),
-    new ThumbObject('PTO', 'images/FRFL/FRFLPTO.jpg', 'null', 'Pointed Toe Cap', true, ba_fl, [6, 0, 1]),
-    new ThumbObject('TOE', 'images/FRFL/FRFLTOE.jpg', 'null', 'Toe Cap', true, ba_fl, [6, 0, 2]),
-    new ThumbObject('PAN', 'images/FRFL/FRFLPAN.jpg', 'null', 'Panneled', true, ba_fl, [6, 0, 3]),
-    new ThumbObject('PPT', 'images/FRFL/FRFLPPT.jpg', 'null', 'Panneled Pointed Toe Cap', true, ba_fl, [6, 0, 4]),
-    new ThumbObject('PTC', 'images/FRFL/FRFLPTC.jpg', 'null', 'Panneled Toe Cap', true, ba_fl, [6, 0, 5])
-]
+        //jQ anim menu
+        var btns = $('#shoeMenu .btn');
+        btns.click(function() {
+            var div = $(this).parent().find('.thumb-container');
+            if ((div[0]).style.display === 'none') {
+                div.slideDown(500);
+            } else {
+                div.slideUp(500);
+            }
+        });
+    }
+
+    redrawMenu([0, 0, 0, 0, 0]);
+    $('.thumb-container:gt(0)').hide();
+
+});
